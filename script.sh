bf3a84e6266f8ec9614335e72daa05efa3dc84b7
djoudj.dev <130213459+djoudj-dev@users.noreply.github.com>
2025-03-04 19:23:50 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
 
-REPO_OWNER="djoudj-dev"
-REPO_NAME="new-repo"  # Remplacez par le nom de votre nouveau repository
-TOKEN="your_personal_access_token"  # Remplacez par votre token GitHub
-
-curl -X POST -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token $TOKEN" \
-  https://api.github.com/repos/$REPO_OWNER/.gitworkflow/dispatches \
-  -d '{"event_type":"refresh"}'
